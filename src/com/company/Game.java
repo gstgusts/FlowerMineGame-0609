e3ddad8b0634a7432bd61ce8a9c00805c373d0f5
Gusts Linkevics <gusts@gst.lv>
2020-08-28 21:03:57 +0300

    Visual resources added

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -3,6 +3,7 @@ package com.company;
 public class Game {
     public static final int FieldSize = 3;
     private FieldStatus[][] board = new FieldStatus[FieldSize][FieldSize];
+    private GeneratedFieldValue[][] values = new GeneratedFieldValue[FieldSize][FieldSize];
 
     public Game() {
         initGame();
@@ -12,8 +13,11 @@ public class Game {
         for (int i = 0; i < FieldSize; i++) {
             for (int j = 0; j < FieldSize; j++) {
                 board[i][j] = FieldStatus.CLOSED;
+                values[i][j] = GeneratedFieldValue.EMPTY;
             }
         }
+
+        generateValues();
     }
 
     public FieldStatus[][] getBoard() {
@@ -22,5 +26,50 @@ public class Game {
 
     public FieldStatus getFieldStatus(int row, int col) {
         return board[row][col];
+    }
+
+    public FieldStatus fieldClicked(int row, int col) {
+        if(board[row][col] != FieldStatus.CLOSED) {
+            return board[row][col];
+        }
+
+        switch (values[row][col]) {
+            case EMPTY -> {
+                return FieldStatus.OPENED;
+            }
+            case MINE -> {
+                return FieldStatus.MINE;
+            }
+            case FLOWER -> {
+                return FieldStatus.FLOWER;
+            }
+        }
+
+        return FieldStatus.OPENED;
+    }
+
+    private void generateValues() {
+       // generate location of mine
+        var row = getRandomNumber(0,3);
+        var col = getRandomNumber(0,3);
+        values[row][col] = GeneratedFieldValue.MINE;
+
+       //generate location for flower
+        var flowerCounter = 0;
+        while (flowerCounter < 2) {
+            var r = getRandomNumber(0, 3);
+            var c = getRandomNumber(0, 3);
+
+            if(values[r][c] == GeneratedFieldValue.MINE || values[r][c] == GeneratedFieldValue.FLOWER) {
+                continue;
+            }
+
+            values[r][c] = GeneratedFieldValue.FLOWER;
+            ++flowerCounter;
+        }
+    }
+
+    private int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
     }
 }
--- src/com/company/GameForm.java
+++ src/com/company/GameForm.java
@@ -2,6 +2,8 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameForm extends JFrame {
 
@@ -9,13 +11,15 @@ public class GameForm extends JFrame {
     private JPanel mainPanel;
     private JPanel buttonPanel;
     private JPanel boardPanel;
+    private JButton btnRestartGame;
+    private JButton[][] boardButtons = new JButton[Game.FieldSize][Game.FieldSize];
 
     public GameForm(String title) throws HeadlessException {
         super(title);
 
         game = new Game();
 
-        this.setSize(300, 350);
+        this.setSize(320, 390);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
     }
@@ -33,42 +37,91 @@ public class GameForm extends JFrame {
         buttonPanel.setBounds(0, 0, 300, 50);
         buttonPanel.setBackground(Color.GRAY);
 
+        btnRestartGame = new JButton("Restart");
+        btnRestartGame.addActionListener(a -> {
+            game.initGame();
+            resetButtonState();
+        });
+
+        buttonPanel.add(btnRestartGame);
+
         boardPanel = new JPanel();
         boardPanel.setBounds(0, 50, 300, 300);
         boardPanel.setBackground(Color.LIGHT_GRAY);
-        boardPanel.setLayout(new GridLayout(3,3));
+        boardPanel.setLayout(null);
+
+        var startXPosition = 0;
+        var startYPosition = 0;
 
         for (int i = 0; i < Game.FieldSize; i++) {
             for (int j = 0; j < Game.FieldSize; j++) {
                 JButton btn = new JButton();
                 btn.setName("btn_"+i+"_"+j);
 
-                switch (game.getFieldStatus(i,j)) {
-                    case FLOWER -> {
-                        btn.setBackground(Color.YELLOW);
-                        break;
-                    }
-                    case MINE -> {
-                        btn.setBackground(Color.RED);
-                        break;
-                    }
-                    case OPENED -> {
-                        btn.setBackground(Color.GRAY);
-                        break;
-                    }
-                    case CLOSED -> {
-                        btn.setBackground(Color.WHITE);
-                        break;
-                    }
-                }
+                btn.setBounds(startXPosition, startYPosition, 100, 100);
+
+                startXPosition += 100;
+
+                updateBtn(btn, game.getFieldStatus(i,j));
+
+                btn.addActionListener(e -> {
+                    var nameParts = btn.getName().split("_");
+
+                    var row = Integer.parseInt(nameParts[1]);
+                    var column = Integer.parseInt(nameParts[2]);
+
+                    var newFieldStat = game.fieldClicked(row, column);
+
+                    updateBtn(btn, newFieldStat);
+
+                });
 
                 boardPanel.add(btn);
+                boardButtons[i][j] = btn;
             }
+
+            startYPosition += 100;
+            startXPosition = 0;
         }
 
         mainPanel.add(buttonPanel);
         mainPanel.add(boardPanel);
 
         this.setContentPane(mainPanel);
+    }
+
+    private void updateBtn(JButton btn, FieldStatus status) {
+        switch (status) {
+            case FLOWER -> {
+                Icon icon = new ImageIcon("C:\\Temp\\flower.PNG");
+                btn.setIcon(icon);
+                btn.setBackground(Color.YELLOW);
+                break;
+            }
+            case MINE -> {
+                Icon icon = new ImageIcon("C:\\Temp\\bomb.PNG");
+                btn.setIcon(icon);
+                btn.setBackground(Color.RED);
+                break;
+            }
+            case OPENED -> {
+                btn.setBackground(Color.GRAY);
+                btn.setIcon(null);
+                break;
+            }
+            case CLOSED -> {
+                btn.setBackground(Color.WHITE);
+                btn.setIcon(null);
+                break;
+            }
+        }
+    }
+
+    private void resetButtonState() {
+        for (int i = 0; i < Game.FieldSize; i++) {
+            for (int j = 0; j < Game.FieldSize; j++) {
+                updateBtn(boardButtons[i][j], game.getFieldStatus(i, j));
+            }
+        }
     }
 }
